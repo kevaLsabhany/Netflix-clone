5e270fa0f7fbcea3204f991e47b2f596147ea474
kevaLsabhany <60646434+kevaLsabhany@users.noreply.github.com>
2021-05-30 07:22:17 +0530

    Update requests.js

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = '<Add API key here>';
+const API_KEY = '<Add API key here>'; // Get the api key from tmdb
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
